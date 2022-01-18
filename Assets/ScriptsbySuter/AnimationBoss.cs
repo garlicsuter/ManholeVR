e1bfdaed77bc6a5459cb8e0fb983063a66e65a16
Mark Suter <msuter@live.elida.k12.oh.us>
2022-01-18 14:42:42 -0500

    animation test complete

--- Assets/ScriptsbySuter/AnimationBoss.cs
+++ Assets/ScriptsbySuter/AnimationBoss.cs
@@ -23,12 +23,30 @@ public class AnimationBoss : MonoBehaviour
     public void Play1Pins()
     {
         theSpeedplate.GetComponent<Animator>().Play("1_PinsOut");
-        Debug.Log("1_PinsOut Played");
+        Debug.Log("1 Pins Out");
     }
 
     public void Play2Expand()
     {
         theSpeedplate.GetComponent<Animator>().Play("2_LeftRightHalvesExpand");
-        Debug.Log("2 expand played");
+        Debug.Log("2 expanded");
+    }
+
+    public void Play3PinsIn()
+    {
+        theSpeedplate.GetComponent<Animator>().Play("3_PinsIn");
+        Debug.Log("3 Pins In");
+    }
+
+    public void Play4CircleSpin()
+    {
+        theSpeedplate.GetComponent<Animator>().Play("4_SpinCircleLock");
+        Debug.Log("4 Sping Circle");
+    }
+
+    public void Play5HandleRelease()
+    {
+        theSpeedplate.GetComponent<Animator>().Play("5_HandleLockRelease");
+        Debug.Log("5 Lock Release");
     }
 }
